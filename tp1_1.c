4d92caf95934580024cf64f977cdab28ef2674e2
Simon Domfrocht <simonbussetti01@gmail.com>
2026-04-03 17:33:18 -0500

    agrego informacion sobre punteros al archivo

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    int variable = 10;
+    int *puntero = &variable;
+
     printf("hola mundo\n");
+
+    printf("El contenido del puntero: %d\n", *puntero);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("La direccion de memoria de la variable: %p\n", &variable);
+    printf("La direccion de memoria del puntero: %p\n", &puntero);
+    printf("El tamanio de memoria utilizado por la variable: %zu\n", sizeof(variable));
+
     return 0;
 }
