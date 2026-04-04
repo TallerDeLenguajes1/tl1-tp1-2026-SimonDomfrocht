9d2feede671584805c30cc77c70398998cd78315
Simon Domfrocht <simonbussetti01@gmail.com>
2026-04-03 19:13:31 -0500

    ejercicio 4 completado

--- tp1_1.c
+++ tp1_1.c
@@ -8,8 +8,8 @@ int main() {
 
     printf("El contenido del puntero: %d\n", *puntero);
     printf("La direccion de memoria almacenada por el puntero: %p\n", puntero);
-    printf("La direccion de memoria de la variable: %p\n", &variable);
-    printf("La direccion de memoria del puntero: %p\n", &puntero);
+    printf("La dieccion de memoria de la variable: %p\n", &variable);
+    printf("la direccion de memoria del puntero: %p\n", &puntero);
     printf("El tamanio de memoria utilizado por la variable: %zu\n", sizeof(variable));
 
     return 0;
